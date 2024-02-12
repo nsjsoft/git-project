e6ed7d224ec2c22a3af48a20a76ac4d5ac678c5e
nsjsoft <nsj0409@gmail.com>
2024-02-12 13:47:04 +0900

    일단 정리

---
+++ nsjsoft.py
@@ -0,0 +1,4 @@
+import sys
+
+input = sys.stdin.readline
+N, M, V = map(int, input().split())
---
+++ second.py
@@ -0,0 +1,8 @@
+# + - * / %
+# ** //
+# and or not in
+# 참: True
+# 거짓: False
+import keyword
+
+print(keyword.kwlist)
