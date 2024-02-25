ccba22ea8b77ffc75aa929814e573345d812596f
나상진 <nsj0409@gmail.com>
2024-02-25 16:25:20 +0900

    mac Entry commit

--- python/hello.py
+++ python/hello.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from datetime import datetime
 #from matplotlib.pyplot import plot
 #from matplotlib.pyplot import show
 #from matplotlib import pyplot
@@ -6,19 +7,29 @@ from tkinter import *
 x = [1,2,3,4,5]
 y = [5,6,7,8,9]
 
+print(datetime.now())
+
 # pyplot.plot(x,y)
 # pyplot.show()
 def alert():
   print("버튼이 눌림")
 
 win = Tk()
-win.geometry("1000x500")
-win.title("temp")
-win.configure(bg='blue') 
+win.geometry("300x100")
+win.title("로또 당첨번호")
+win.option_add("*Font", "나눔고딕 16")
+win.configure(bg='red') 
 
-btn = Button(win, text="버튼")
+ent = Entry(win)
+ent.pack()
+
+def ent_p():
+  a = ent.get()
+  print(a)
+
+btn = Button(win, text="로또 당첨번호 확인")
 btn.config(width=20, height=20)
-btn.config(command=alert)
+btn.config(command=ent_p)
 btn.pack()
   
 win.mainloop()
